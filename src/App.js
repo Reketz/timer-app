5df0f73efa1d18b77546aad480d12545e013ebf4
Guilherme Estevao <guilhermewilliann@hotmail.com>
2023-12-03 10:47:19 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Stopwatch() {
+    const [startTime, setStartTime] = useState(null);
+    const [now, setNow] = useState(null);
+    const intervalRef = useRef(null);
+
+    function handleStart() {
+        setStartTime(Date.now());
+        setNow(Date.now());
+
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(() => {
+            setNow(Date.now());
+        }, 10);
+    }
+
+    function handleStop() {
+        clearInterval(intervalRef.current);
+    }
+
+    let secondsPassed = 0;
+    if (startTime != null && now != null) {
+        secondsPassed = (now - startTime) / 1000;
+    }
+
+    const style = {fontSize: "24px", margin: "12px"};
+
+    return (
+        <div className='App'>
+            <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+            <button style={style} onClick={handleStart}>
+                Start
+            </button>
+            <button style={style} onClick={handleStop}>
+                Stop
+            </button>
+        </div>
+    );
 }
-
-export default App;
